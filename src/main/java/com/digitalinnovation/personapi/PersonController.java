748da13923862067a14b5c872038c022a77cfca0
Diego <diegorf55@gmail.com>
2021-09-20 01:44:21 -0300

    created PersonService class and modifed PersonController

--- src/main/java/com/digitalinnovation/personapi/PersonController.java
+++ src/main/java/com/digitalinnovation/personapi/PersonController.java
@@ -3,27 +3,25 @@ package com.digitalinnovation.personapi;
 import com.digitalinnovation.personapi.dto.MessageResponseDTO;
 import com.digitalinnovation.personapi.entity.Person;
 import com.digitalinnovation.personapi.repository.PersonRepository;
+import com.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/V1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
+    private PersonService personService;
 
     @Autowired
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
-
     @PostMapping
-    public MessageResponseDTO createPerson(@RequestBody Person person){
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Create Person " + savedPerson.getFirstName())
-                .build();
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+    return personService.createPerson(person);
     }
 }
