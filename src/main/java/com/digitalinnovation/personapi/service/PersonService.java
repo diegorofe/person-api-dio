9b0bf5d061741c8a0118c6440bec0e939a74ad65
Diego <diegorf55@gmail.com>
2021-09-22 13:36:45 -0300

    Developed list all method

--- src/main/java/com/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/com/digitalinnovation/personapi/service/PersonService.java
@@ -9,8 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service //especifica um serviço
 public class PersonService {
+
 
     private PersonRepository personRepository;
 
@@ -31,5 +35,11 @@ public class PersonService {
                 .builder()
                 .message("Create Person " + savedPerson.getFirstName())
                 .build();
+    }
+    public List<PersonDTO> listAll(){
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
